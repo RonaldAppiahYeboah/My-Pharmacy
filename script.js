7823bae9b92e8d843178e4776339217fe3a3bb02
Chidori Baby <appiahyeboahronald@gmail.com>
2025-04-06 15:31:15 -0700

    First Commit - Adding files to repo

---
+++ script.js
@@ -0,0 +1,27 @@
+var groupContainer = document.getElementById("groupContainer");
+if (groupContainer) {
+  groupContainer.addEventListener("click", function (e) {
+    // Please sync "02 inventory home" to the project
+  });
+}
+
+var groupContainer1 = document.getElementById("groupContainer1");
+if (groupContainer1) {
+  groupContainer1.addEventListener("click", function (e) {
+    // Please sync "04reports" to the project
+  });
+}
+
+var groupContainer2 = document.getElementById("groupContainer2");
+if (groupContainer2) {
+  groupContainer2.addEventListener("click", function (e) {
+    // Please sync "05 configurations" to the project
+  });
+}
+
+var groupContainer = document.getElementById("groupContainer");
+if (groupContainer) {
+  groupContainer.addEventListener("click", function (e) {
+    // Please sync "05 configurations" to the project
+  });
+}
